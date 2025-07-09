6c44d4e65d4b2aadb696eb53179ff2bc104bb8f8
gurde <gurde@192.168.2.18>
2025-07-08 20:12:18 -0400

    push first time

---
+++ MidTerm_quetion3/src/midterm_quetion3/MidTerm_quetion3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package midterm_quetion3;
+
+/**
+ *
+ * @author gurde
+ */
+public class MidTerm_quetion3 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
